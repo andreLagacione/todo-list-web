3436cfba946f92c4208d64871327dc14a8911323
André Lagacione <lagacioneandre@gmail.com>
2021-06-03 13:52:12 -0300

    add toastify library

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,18 +3,24 @@ import Header from '../../components/header';
 import TaskInput from '../../components/task-input';
 import Tasklist from '../../components/task-list';
 import { Container, Content } from './styles';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Dashboard: React.FC = () => {
 
     return (
-        <Container>
-            <Header />
+        <>
+            <Container>
+                <Header />
 
-            <Content>
-                <TaskInput />
-                <Tasklist />
-            </Content>
-        </Container>
+                <Content>
+                    <TaskInput />
+                    <Tasklist />
+                </Content>
+            </Container>
+
+            <ToastContainer />
+        </>
     )
 };
 
