42a438a723db14690328627c4d3c9afa8c40cb1d
André Lagacione <lagacioneandre@gmail.com>
2021-06-07 16:04:42 -0300

    add filters

--- src/components/task-list/index.tsx
+++ src/components/task-list/index.tsx
@@ -1,38 +1,188 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskItem from '../task-item';
-import { Container } from './styles';
-import { FaSortAlphaDown, FaSortAlphaDownAlt } from 'react-icons/fa';
+import { Container, Filter } from './styles';
+import {
+    FaSortAlphaDown,
+    FaSortAlphaDownAlt,
+    FaCheck,
+    FaClock,
+    FaCheckDouble,
+    FaTasks,
+    FaListUl,
+} from 'react-icons/fa';
 import Loader from '../loader';
 import { useTask } from '../../hooks/task';
 
+enum OrderByFilter {
+    description_desc = 'DESCRIPTION_DESC',
+    description_asc = 'DESCRIPTION_ASC',
+    created_at = 'CREATED_AT',
+    completed_at = 'COMPLETED_AT',
+}
+
+enum FilterByFilter {
+    all = 'ALL',
+    complete = 'COMPLETE',
+    incomplete = 'INCOMPLETE',
+}
+
 const Tasklist: React.FC = () => {
-    const { tasks } = useTask();
+    const { tasks, controlLoaderTaskList, FilterTask } = useTask();
+    const [orderByFilter, setOrderByFilter] = useState<OrderByFilter>(OrderByFilter.description_asc);
+    const [filterByFilter, setFilterByFilter] = useState<FilterByFilter>(FilterByFilter.all);
+
+    const handleOrderBy = (event: any) => {
+        setOrderByFilter(event.target.value);
+        filterTasks(filterByFilter, event.target.value);
+    }
+
+    const handleFilterBy = (event: any) => {
+        setFilterByFilter(event.target.value);
+        filterTasks(event.target.value, orderByFilter);
+    }
+
+    const filterTasks = (filterBy: FilterByFilter, orderBy: OrderByFilter) => {
+        FilterTask(filterBy, orderBy);
+    }
 
     useEffect(() => {
-        console.log(tasks);
-    }, [tasks]);
+        
+    }, [tasks, controlLoaderTaskList]);
 
     return (
         <Container>
-            <h2>
-                Tasks
-
-                <FaSortAlphaDown size={14} className="icon" />
-                <FaSortAlphaDownAlt size={14} className="icon" />
-            </h2>
-
-            {/* <Loader /> */}
+            <h2>Tasks</h2>
 
             {
-                tasks.map((task, index) => (
-                    <TaskItem task={task} lastTask={tasks.length === index + 1} />
-                ))
-            }
+                controlLoaderTaskList ? <Loader />
+                :
+                    <>
+                        <Filter>
+                            <div>
+                                <strong>Order by:</strong>
 
-            <label htmlFor="hideCompleted">
-                Hide completed
-                <input type="checkbox" name="hideCompleted" id="hideCompleted" value="true" />
-            </label>
+                                <div className="options">
+                                    <label htmlFor="description_asc">
+                                        <input
+                                            type="radio"
+                                            name="orderBy"
+                                            id="description_asc"
+                                            value={OrderByFilter.description_asc}
+                                            checked={orderByFilter === OrderByFilter.description_asc}
+                                            onChange={handleOrderBy}
+                                        />
+                                        Description
+                                        <FaSortAlphaDown size={14} className="icon" />
+                                    </label>
+
+                                    <label htmlFor="description_desc">
+                                        <input
+                                            type="radio"
+                                            name="orderBy"
+                                            id="description_desc"
+                                            value={OrderByFilter.description_desc}
+                                            checked={orderByFilter === OrderByFilter.description_desc}
+                                            onChange={handleOrderBy}
+                                        />
+                                        Description
+                                        <FaSortAlphaDownAlt size={14} className="icon" />
+                                    </label>
+
+                                    <label htmlFor="created_at">
+                                        <input
+                                            type="radio"
+                                            name="orderBy"
+                                            id="created_at"
+                                            value={OrderByFilter.created_at}
+                                            checked={orderByFilter === OrderByFilter.created_at}
+                                            onChange={handleOrderBy}
+                                        />
+                                        Create
+                                        <FaClock size={14} className="icon" />
+                                    </label>
+
+                                    <label htmlFor="completed_at">
+                                        <input
+                                            type="radio"
+                                            name="orderBy"
+                                            id="completed_at"
+                                            value={OrderByFilter.completed_at}
+                                            checked={orderByFilter === OrderByFilter.completed_at}
+                                            onChange={handleOrderBy}
+                                        />
+                                        Complete
+                                        <FaCheck size={14} className="icon" />
+                                    </label>
+                                </div>
+                            </div>
+                            
+                            <div>
+                                <strong>Filter by:</strong>
+
+                                <div className="options">
+                                    <label htmlFor="all">
+                                        <input
+                                            type="radio"
+                                            name="filterBy"
+                                            id="all"
+                                            value={FilterByFilter.all}
+                                            checked={filterByFilter === FilterByFilter.all}
+                                            onChange={handleFilterBy}
+                                        />
+                                        All
+                                        <FaCheckDouble size={14} className="icon" />
+                                    </label>
+
+                                    <label htmlFor="complete">
+                                        <input
+                                            type="radio"
+                                            name="filterBy"
+                                            id="complete"
+                                            value={FilterByFilter.complete}
+                                            checked={filterByFilter === FilterByFilter.complete}
+                                            onChange={handleFilterBy}
+                                        />
+                                        Complete
+                                        <FaTasks size={14} className="icon" />
+                                    </label>
+
+                                    <label htmlFor="incomplete">
+                                        <input
+                                            type="radio"
+                                            name="filterBy"
+                                            id="incomplete"
+                                            value={FilterByFilter.incomplete}
+                                            checked={filterByFilter === FilterByFilter.incomplete}
+                                            onChange={handleFilterBy}
+                                        />
+                                        Incomplete
+                                        <FaListUl size={14} className="icon" />
+                                    </label>
+                                </div>
+                            </div>
+                        </Filter>
+
+                        {
+                            tasks.length ?
+                            tasks.map((task, index) => (
+                                <TaskItem task={task} lastTask={tasks.length === index + 1} />
+                            ))
+                            :
+                            <div className="alert">No tasks found!</div>
+                        }
+
+                        {
+                            tasks.length ?
+                            <label htmlFor="hideCompleted">
+                                Hide completed
+                                <input type="checkbox" name="hideCompleted" id="hideCompleted" value="true" />
+                            </label>
+                            :
+                            ''
+                        }
+
+                    </>
+            }
         </Container>
     )
 };
