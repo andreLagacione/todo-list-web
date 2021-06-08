284fc4bcc115f1bda384d79245912b768e0aa2f2
André Lagacione <lagacioneandre@gmail.com>
2021-06-07 21:00:36 -0300

    edit task description

--- src/components/task-input/index.tsx
+++ src/components/task-input/index.tsx
@@ -1,19 +1,44 @@
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { useTask } from '../../hooks/task';
 import { Container } from './styles';
 
 const TaskInput: React.FC = () => {
     const [taskName, setTaskName] = useState<string>('');
-    const { createTask } = useTask();
+    const [buttonTitle, setButtonTitle] = useState<string>('Create');
+    const { createTask, updateTask, editTaskTitle } = useTask();
+
+    useEffect(() => {
+        if (editTaskTitle && editTaskTitle.description) {
+            setButtonTitle('Update');
+            setTaskName(editTaskTitle.description);
+        } else {
+            resetValues();
+        }
+
+    }, [editTaskTitle]);
 
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
         const value = taskName.trim();
-        if (value) {
-            createTask(value);
-            setTaskName('');
+        if (!value) {
+            return;
         }
+
+        if (editTaskTitle && editTaskTitle.description) {
+            editTaskTitle.description = taskName;
+            updateTask(editTaskTitle);
+            resetValues();
+            return;
+        }
+
+        createTask(value);
+        resetValues();
     };
+
+    const resetValues = () => {
+        setTaskName('');
+        setButtonTitle('Create');
+    }
 
     return (
         <Container>
@@ -27,7 +52,7 @@ const TaskInput: React.FC = () => {
                     onChange={(e: any) => setTaskName(e.target.value)}
                 />
 
-                <button type="submit" disabled={!taskName}>Create</button>
+                <button type="submit" disabled={!taskName.trim()}>{buttonTitle}</button>
             </form>
         </Container>
     )
